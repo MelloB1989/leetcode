94041bc61fd5404d40d52d325fb76ffd9f9b3cf7
Kartik deshmukh <kartikdd90@gmail.com>
2024-10-26 23:31:27 +0530

    Update

---
+++ easy/kThCharacter.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func kthCharacter(k int) byte {
+	init := "a"
+	for len(init) < k {
+		init += genDo(init)
+		// fmt.Println(init)
+	}
+	return init[k-1]
+}
+
+func genDo(s string) string {
+	r := ""
+	for i := 0; i < len(s); i++ {
+		c := int(s[i])
+		r += string(c + 1)
+		if c == 122 {
+			r += string(97)
+		}
+	}
+	return r
+}
+
+func main() {
+	fmt.Println(kthCharacter(5))
+}
