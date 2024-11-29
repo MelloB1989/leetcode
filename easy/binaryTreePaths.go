ff275fa291783b3fef8055089f2cfd33e5f77995
Kartik deshmukh <kartikdd90@gmail.com>
2024-11-29 14:02:30 +0530

    Update

--- easy/binaryTreePaths.go
+++ easy/binaryTreePaths.go
@@ -13,23 +13,24 @@ type TreeNode struct {
 
 func binaryTreePaths(root *TreeNode) []string {
 	paths := []string{}
-	var b func(r *TreeNode)
-	b = func(r *TreeNode) {
-		if r.Left != nil {
-			v := strconv.Itoa(r.Val)
-			paths = append(paths, v)
-			b(r.Left)
-		} else if r.Right != nil {
-			v := strconv.Itoa(r.Val)
-			paths = append(paths, v)
-			b(r.Right)
+	var b func(r *TreeNode, path string)
+	b = func(r *TreeNode, path string) {
+		if r == nil {
+			return
 		}
-		v := strconv.Itoa(r.Val)
-		paths = append(paths, v)
-		b(root.Right)
+		if len(path) > 0 {
+			path += "->"
+		}
+		path += strconv.Itoa(r.Val)
+		if r.Left == nil && r.Right == nil {
+			paths = append(paths, path)
+		} else {
+			b(r.Left, path)
+			b(r.Right, path)
+		}
 	}
 
-	b(root)
+	b(root, "")
 
 	return paths
 }
