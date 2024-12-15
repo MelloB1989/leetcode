4fd2f29e2175775654623d2749a3a44fa9723b2a
Kartik Deshmukh <kartikdd90@gmail.com>
2024-12-15 18:22:48 +0530

    Update

--- medium/searching/searchInRotatedArray.go
+++ medium/searching/searchInRotatedArray.go
@@ -5,12 +5,30 @@ import "fmt"
 func search(nums []int, target int) int {
 	right := len(nums) - 1
 	left := 0
-	for left < right {
-
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		fmt.Println(nums[mid])
+		if nums[left] <= nums[mid] {
+			if nums[left] <= target && target < nums[mid] { //Lies on Left side
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
 	}
-	return left
+	return -1
 }
 
 func main() {
-	fmt.Println(search([]int{4, 5, 6, 7, 1, 2, 3}, 1))
+	fmt.Println(search([]int{5, 1, 3}, 3))
+	// fmt.Println(search([]int{4, 5, 6, 7, 1, 2, 3}, 1))
 }
