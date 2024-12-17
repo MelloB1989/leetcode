3551941d06145fda46cc5f5c17b913127198b65a
Kartik Deshmukh <kartikdd90@gmail.com>
2024-12-17 14:27:07 +0530

    Update

--- java/ArrayPartitioning.java
+++ java/ArrayPartitioning.java
@@ -7,22 +7,22 @@ public class ArrayPartitioning {
         int diff = Integer.MAX_VALUE;
         int n = arr.length;
         int[] preSum = new int[n + 1];
-        for (int i = 1; i < n; i++) {
-            preSum[i] = arr[i] + arr[i - 1];
+        for (int i = 1; i <= n; i++) {
+            preSum[i] = preSum[i - 1] + arr[i - 1];
         }
 
         for (int i = 1; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
                 for (int k = j + 1; k < n; k++) {
-                    int w = preSum[i - 1];
-                    int x = preSum[j - 1] - w;
-                    int y = preSum[k - 1] - (x + w);
-                    int z = preSum[n - 1] - (y + x + w);
+                    int w = preSum[i];
+                    int x = preSum[j] - preSum[i];
+                    int y = preSum[k] - preSum[j];
+                    int z = preSum[n] - preSum[k];
 
                     int maxSum = Math.max(Math.max(w, x), Math.max(y, z));
                     int minSum = Math.min(Math.min(w, x), Math.min(y, z));
 
-                    System.out.println(maxSum + " " + minSum);
+                    // System.out.println(maxSum + " " + minSum);
 
                     diff = Math.min(diff, maxSum - minSum);
                 }
