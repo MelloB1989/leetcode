0d9f0bba711b3fc37189f93c645c3faad65ee87a
Kartik Deshmukh <kartikdd90@gmail.com>
2025-01-27 22:52:42 +0530

    Update

---
+++ easy/125.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func isPalindrome(s string) bool {
+	runes := []rune(s)
+	left, right := 0, len(runes)-1
+
+	for left < right {
+		for left < right && !unicode.IsLetter(runes[left]) && !unicode.IsDigit(runes[left]) {
+			left++
+		}
+		for left < right && !unicode.IsLetter(runes[right]) && !unicode.IsDigit(runes[right]) {
+			right--
+		}
+		if unicode.ToLower(runes[left]) != unicode.ToLower(runes[right]) {
+			return false
+		}
+		left++
+		right--
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(isPalindrome("0P"))
+}
