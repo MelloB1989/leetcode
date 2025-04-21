9ea6f987185e7e5a00e84ba93004869f277df65a
Kartik Deshmukh <kartikdd90@gmail.com>
2025-04-22 03:09:30 +0530

    Update

--- medium/3523.go
+++ medium/3523.go
@@ -5,10 +5,18 @@ import (
 )
 
 func maximumPossibleSize(nums []int) int {
-
+	c := 0
+	locMax := nums[0]
+	for i := 0; i < len(nums); i++ {
+		if nums[i] >= locMax {
+			locMax = nums[i]
+			c++
+		}
+	}
+	return c
 }
 
 func main() {
-	nums := []int{4, 2, 5, 3, 5}
+	nums := []int{19, 80, 63, 74}
 	fmt.Println(maximumPossibleSize(nums))
 }
