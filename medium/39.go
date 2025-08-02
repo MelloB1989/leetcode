6727d149bc8fd1add487e6304b1798a3918e6601
Kartik Deshmukh <kartikdd90@gmail.com>
2025-08-02 16:18:46 +0530

    Update

--- medium/39.go
+++ medium/39.go
@@ -1,1 +1,34 @@
 package main
+
+import "fmt"
+
+func combinationSum(candidates []int, target int) [][]int {
+	result := [][]int{}
+	sumSet := []int{}
+	currentSum := 0
+
+	var b func(e int)
+	b = func(e int) {
+		if currentSum == target {
+			s := make([]int, len(sumSet))
+			copy(s, sumSet)
+			result = append(result, s)
+		}
+		for i := e; i < len(candidates); i++ {
+			if currentSum+candidates[i] <= target {
+				currentSum += candidates[i]
+				sumSet = append(sumSet, candidates[i])
+				b(i)
+				currentSum -= sumSet[len(sumSet)-1]
+				sumSet = sumSet[:len(sumSet)-1]
+			}
+		}
+	}
+	b(0)
+	return result
+}
+
+func main() {
+	fmt.Println(combinationSum([]int{2, 3, 6, 7}, 7))
+	// fmt.Println(combinationSum([]int{2, 3, 5}, 8))
+}
