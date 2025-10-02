756a5ac7dda52ecc06e2feafcb0b38d4ff8a061d
Kartik Deshmukh <kartikdd90@gmail.com>
2025-10-03 01:07:35 +0530

    Update

--- medium/200.go
+++ medium/200.go
@@ -3,26 +3,44 @@ package main
 import "fmt"
 
 func numIslands(grid [][]byte) int {
-	x, y := 0, 0
-	rows, cols := len(grid), len(grid[0])
-	for x < rows && y < cols {
+	var dfs func(r, c int)
 
+	dfs = func(r, c int) {
+		if r < 0 || c < 0 || r >= len(grid) || c >= len(grid[0]) || grid[r][c] == '0' {
+			return
+		}
+		grid[r][c] = '0'
+		dfs(r+1, c)
+		dfs(r-1, c)
+		dfs(r, c+1)
+		dfs(r, c-1)
 	}
+
+	ans := 0
+	for r, row := range grid {
+		for c := range row {
+			if grid[r][c] == '1' {
+				ans++
+				dfs(r, c)
+			}
+		}
+	}
+	return ans
 }
 
 func main() {
-	grid := [][]byte{
-		{'1', '1', '1', '1', '0'},
-		{'1', '1', '0', '1', '0'},
-		{'1', '1', '0', '0', '0'},
-		{'0', '0', '0', '0', '0'},
-	}
-	// grid1 := [][]byte{
+	// grid := [][]byte{
+	// 	{'1', '1', '1', '1', '0'},
+	// 	{'1', '1', '0', '1', '0'},
 	// 	{'1', '1', '0', '0', '0'},
-	// 	{'1', '1', '0', '0', '0'},
-	// 	{'0', '0', '1', '0', '0'},
-	// 	{'0', '0', '0', '1', '1'},
+	// 	{'0', '0', '0', '0', '0'},
 	// }
+	grid := [][]byte{
+		{'1', '1', '0', '0', '0'},
+		{'1', '1', '0', '0', '0'},
+		{'0', '0', '1', '0', '0'},
+		{'0', '0', '0', '1', '1'},
+	}
 
 	numberOfIslands := numIslands(grid)
 	fmt.Println(numberOfIslands)
