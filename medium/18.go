b5752049c1476d002e44acf861fdaff3575662d0
Kartik Deshmukh <kartikdd90@gmail.com>
2026-03-01 16:33:56 +0530

    Update

--- medium/18.go
+++ medium/18.go
@@ -7,46 +7,61 @@ import (
 
 func fourSum(nums []int, target int) [][]int {
 	sort.Ints(nums)
-	length := len(nums)
+	n := len(nums)
 	result := [][]int{}
-	for i := 0; i < length-3; i++ {
-		if i > 0 && nums[i] == nums[i-1] {
+
+	for a, _ := range nums {
+		if a == n-3 {
+			break
+		}
+		// Skip dups
+		if a > 0 && nums[a] == nums[a-1] {
 			continue
 		}
-		for j := i + 1; j < length-2; j++ {
-			if j > i+1 && nums[j] == nums[j-1] {
+
+		b := a + 1
+
+		for b < n-2 {
+			// Skip dups
+			if b > a+1 && nums[b] == nums[b-1] {
+				b++
 				continue
 			}
-			l, r := j+1, length-1
-			for l < r {
-				a, b, c, d := nums[i], nums[j], nums[l], nums[r]
-				sum := a + b + c + d
-				if sum == target {
-					result = append(result, []int{a, b, c, d})
 
-					for l < r && nums[l] == nums[l+1] {
-						l++
-					}
-					for l < r && nums[r] == nums[r-1] {
-						r--
-					}
-					l++
+			l, r := b+1, n-1
+
+			for l < r {
+				sum := nums[a] + nums[b] + nums[l] + nums[r]
+
+				if sum > target {
 					r--
 				} else if sum < target {
 					l++
 				} else {
+					result = append(result, []int{nums[a], nums[b], nums[l], nums[r]})
+
+					l++
 					r--
+
+					for l < r && nums[l] == nums[l-1] {
+						l++
+					}
+
+					for l < r && nums[r] == nums[r+1] {
+						r--
+					}
 				}
 			}
+			b++
 		}
 	}
 	return result
 }
 
 func main() {
-	// nums := []int{1, 0, -1, 0, -2, 2}
-	nums := []int{2, 2, 2, 2, 2}
-	target := 8
+	nums := []int{1, 0, -1, 0, -2, 2}
+	// nums := []int{2, 2, 2, 2, 2}
+	target := 0
 
 	fmt.Println(fourSum(nums, target))
 }
